f9cf10c4f19f298ff12e54c075e58a5970c04a45
Nguyen Minh Tuan <tnowad@gmail.com>
2026-05-30 04:30:58 +0700

    Fix: wrap DevIntegrationHookListener in RedmineDevIntegration module
    Zeitwerk expects class RedmineDevIntegration::DevIntegrationHookListener
    for file lib/redmine_dev_integration/dev_integration_hook_listener.rb

--- lib/redmine_dev_integration/dev_integration_hook_listener.rb
+++ lib/redmine_dev_integration/dev_integration_hook_listener.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class DevIntegrationHookListener < Redmine::Hook::ViewListener
-  def view_issues_sidebar_issues_bottom(context = {})
-    @issue = context[:issue] || context[:controller]&.instance_variable_get(:@issue)
-    return '' unless @issue
+module RedmineDevIntegration
+  class DevIntegrationHookListener < Redmine::Hook::ViewListener
+    def view_issues_sidebar_issues_bottom(context = {})
+      @issue = context[:issue] || context[:controller]&.instance_variable_get(:@issue)
+      return '' unless @issue
 
-    controller = context[:controller]
-    controller.send(:render_to_string, {
-      partial: 'issues/sidebar/dev_integration_summary',
-      locals: { issue: @issue }
-    })
+      controller = context[:controller]
+      controller.send(:render_to_string, {
+        partial: 'issues/sidebar/dev_integration_summary',
+        locals: { issue: @issue }
+      })
+    end
   end
 end
